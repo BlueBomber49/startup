30073343b0b89e08df01fb3ec98dfdb16f920515
Tony Johnson <tonyjohnson15691@gmail.com>
2024-02-21 11:47:25 -0700

    Updated(create.js) toppings array now updates t/f

---
+++ create.js
@@ -0,0 +1,32 @@
+let toppings = {pepperoni: false, pineapple: false, canadian_bacon: false, peppers: false, jalapeno: false, sausage: false}
+
+let cards = document.getElementsByClassName("Ingredient_card");
+
+for(i = 0; i < cards.length; i++){
+    cards[i].addEventListener("click", function(event) {editToppings(event)});
+}
+
+function editToppings(event) {
+    let current_card;
+    console.log(event)
+    if(event.target.id === "Pepperoni" || event.target.parentNode.id === "Pepperoni"){
+        toppings.pepperoni = !toppings.pepperoni;
+    }
+    if(event.target.id === "Pineapple" || event.target.parentNode.id === "Pineapple"){
+        toppings.pineapple = !toppings.pineapple;
+    }
+    if(event.target.id === "Canadian_Bacon" || event.target.parentNode.id === "Canadian_Bacon"){
+        toppings.canadian_bacon = !toppings.canadian_bacon;
+    }
+    if(event.target.id === "Peppers" || event.target.parentNode.id === "Peppers"){
+        toppings.peppers = !toppings.peppers;
+    }
+    if(event.target.id === "Jalapeno" || event.target.parentNode.id === "Jalapeno"){
+        toppings.jalapeno = !toppings.jalapeno;
+    }
+    if(event.target.id === "Sausage" || event.target.parentNode.id === "Sausage"){
+        toppings.sausage = !toppings.sausage;
+    }
+    console.log(toppings);
+
+}
