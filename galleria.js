c94a15ae4b142fe76ba7a67233f95ebbd2bb1237
Tony Johnson <tonyjohnson15691@gmail.com>
2024-02-28 15:12:47 -0700

    Fixed(images) to be pulled from assets dir

--- galleria.js
+++ galleria.js
@@ -26,27 +26,27 @@ for(i in pizzaList){
 }
 
 function pizzaToCard (displayPizza){
-    let html = "<div class='card'><div class='Canvas'><img class='Crust' src='Pizza canvas.png'/>";
+    let html = "<div class='card'><div class='Canvas'><img class='Crust' src='Assets/Pizza canvas.png'/>";
 
     let toppings = displayPizza.getToppings()
 
     if(toppings.canadian_bacon){
-        html += "<img id='img_Canadian_Bacon' class='Ingredient' src='Canadian_Bacon_topping.png'/>"
+        html += "<img id='img_Canadian_Bacon' class='Ingredient' src='Assets/Canadian_Bacon_topping.png'/>"
     }
     if(toppings.peppers){
-        html += "<img id='img_Peppers' class='Ingredient' src='Peppers_topping.png'/>"
+        html += "<img id='img_Peppers' class='Ingredient' src='Assets/Peppers_topping.png'/>"
     }
     if(toppings.jalapeno){
-        html += "<img id='img_Jalapeno' class='Ingredient' src='Jalapeno_topping.png'/>"
+        html += "<img id='img_Jalapeno' class='Ingredient' src='Assets/Jalapeno_topping.png'/>"
     }
     if(toppings.pineapple){
-        html += "<img id='img_Pineapple' class='Ingredient' src='Pineapple_topping.png'/>"
+        html += "<img id='img_Pineapple' class='Ingredient' src='Assets/Pineapple_topping.png'/>"
     }
     if(toppings.sausage){
-        html += "<img id='img_Sausage' class='Ingredient' src='Sausage_topping.png'/>"
+        html += "<img id='img_Sausage' class='Ingredient' src='Assets/Sausage_topping.png'/>"
     }
     if(toppings.pepperoni){
-        html += "<img id='img_Pepperoni' class='Ingredient' src='Pepperoni_topping.png'/>"
+        html += "<img id='img_Pepperoni' class='Ingredient' src='Assets/Pepperoni_topping.png'/>"
     }
 
     html += "</div><p>" + displayPizza.getDescription() + "</p></div>"
