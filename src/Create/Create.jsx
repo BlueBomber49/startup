1cd7b96613bda55c8454e358180f7ed2e986bbf1
Tony Johnson <tonyjohnson15691@gmail.com>
2024-04-11 13:42:40 -0600

    updated(create.jsx) so that toppings are stored within a variable

--- src/Create/Create.jsx
+++ src/Create/Create.jsx
@@ -4,15 +4,32 @@ import './Create.css'
 import {pizza} from './Pizza.js'
 
 export function Create() {
+   
+    const [pepperoniState, setPepperoni] = React.useState(false);
+    const [pineappleState, setPineapple] = React.useState(false);
+    const [canadianBaconState, setBacon] = React.useState(false);
+    const [peppersState, setPeppers] = React.useState(false);
+    const [jalapenoState, setJalapeno] = React.useState(false);
+    const [sausageState, setSausage] = React.useState(false);
 
-    let toppings = {pepperoni: false, pineapple: false, canadian_bacon: false, peppers: false, jalapeno: false, sausage: false}
+    
+    let toppings = {pepperoni: pepperoniState, pineapple: pineappleState, canadian_bacon: canadianBaconState,
+        peppers: peppersState, jalapeno: jalapenoState, sausage: sausageState}
 
-    const [pepperoni, setPepperoni] = React.useState(false);
-    const [pineapple, setPineapple] = React.useState(false);
-    const [canadianBacon, setBacon] = React.useState(false);
-    const [peppers, setPeppers] = React.useState(false);
-    const [jalapeno, setJalapeno] = React.useState(false);
-    const [sausage, setSausage] = React.useState(false);
+
+    function modifyIngredient(type){
+        if(type == 'pepperoni'){
+            setPepperoni(!pepperoniState);
+        } else if(type == 'peppers'){
+            setPeppers(!peppersState);
+        } else if(type == 'pineapple'){
+            setPineapple(!pineappleState);
+        } else if(type == 'canadianBacon'){
+            setBacon(!canadianBaconState);
+        } else if(type == 'sausage'){
+            setSausage(!sausageState);
+        }
+    }
 
     const navigate = useNavigate();
 
@@ -23,13 +40,13 @@ export function Create() {
   return (
 <main>
         <div className="left_column">
-            <div className ="Ingredient_card" id="Pepperoni" onClick={() => setPepperoni(!pepperoni)}>
+            <div className ="Ingredient_card" id="Pepperoni" onClick={() => modifyIngredient('pepperoni')}>
                 <img src="https://www.usatoday.com/gcdn/media/2021/02/02/USATODAY/usatsports/247WallSt.com-247WS-835626-imageforentry145.jpg?crop=1365,768,x0,y0"/>
                 Pepperoni</div>
-            <div className ="Ingredient_card" id="Pineapple" onClick={() => setPineapple(!pineapple)}>
+            <div className ="Ingredient_card" id="Pineapple" onClick={() => modifyIngredient('pineapple')}>
                 <img src="https://as2.ftcdn.net/v2/jpg/02/71/58/03/1000_F_271580324_bdy4peA9LhUWaECDQFBPG9sBqtD5SoTt.jpg"/>
                 Pineapple</div>
-            <div className ="Ingredient_card" id="Canadian_Bacon" onClick={() => setBacon(!canadianBacon)}>
+            <div className ="Ingredient_card" id="Canadian_Bacon" onClick={() => modifyIngredient('canadianBacon')}>
                 <img src="https://qph.cf2.quoracdn.net/main-qimg-5d77ce42480e9884dc123d6e0ba62219-lq"/>
                 Canadian Bacon</div>
         </div>
@@ -49,22 +66,22 @@ export function Create() {
             <div className="Canvas">
                 <img id="Crust" src="Assets/Pizza canvas.png"/>
 
-                {canadianBacon && (
+                {canadianBaconState && (
                 <img id="img_Canadian_Bacon" className="hotIngredient" src="Assets/Canadian_Bacon_topping.png"/>
                 )}
-                {peppers && (
+                {peppersState && (
                 <img id="img_Peppers" className="hotIngredient" src="Assets/Peppers_topping.png"/>
                 )}
-                {jalapeno && (
+                {jalapenoState && (
                 <img id="img_Jalapeno" className="hotIngredient" src="Assets/Jalapeno_topping.png"/>
                 )}
-                {pineapple && (
+                {pineappleState && (
                 <img id="img_Pineapple" className="hotIngredient" src="Assets/Pineapple_topping.png"/>
                 )}
-                {sausage && (
+                {sausageState && (
                 <img id="img_Sausage" className="hotIngredient" src="Assets/Sausage_topping.png"/>
                 )}
-                {pepperoni && (
+                {pepperoniState && (
                 <img id="img_Pepperoni" className="hotIngredient" src="Assets/Pepperoni_topping.png"/>
                 )}
             </div>
@@ -77,14 +94,14 @@ export function Create() {
         </div>
 
         <div className = "right_column">
-            <div className ="Ingredient_card" id="Jalapeno" onClick={() => setJalapeno(!jalapeno)}>
+            <div className ="Ingredient_card" id="Jalapeno" onClick={() => modifyIngredient('jalapeno')}>
                 <img src="https://rolandfoods.com/product_images/45798-jalapeo-pepper-nacho-sliced-raw-600.png" />
                 Jalapeno peppers</div>
-            <div className ="Ingredient_card" id="Sausage" onClick={() => setSausage(!sausage)}>
+            <div className ="Ingredient_card" id="Sausage" onClick={() => modifyIngredient('sausage')}>
                 <img src="https://scm-assets.constant.co/scm/ahold/9a888e72fca371869e751ab34491d024/4790c985-d46a-475d-9d83-c85f3b0449bc.jpg"/>
                 Italian sausage
             </div>
-            <div className ="Ingredient_card" id="Peppers" onClick={() => setPeppers(!peppers)}>
+            <div className ="Ingredient_card" id="Peppers" onClick={() => modifyIngredient('peppers')}>
                 <img src="https://images.eatthismuch.com/img/516766_basic_bob_346a16bc-c8b3-42ce-866f-1f59bd24985d.png"/>
                 Peppers</div>
 
